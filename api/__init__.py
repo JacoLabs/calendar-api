8edca0f36a2cc3e75304cf57ac686bfa5f6b84a1
Gwen1987 <gwen.seymour@gmail.com>
2025-09-25 21:38:09 -0400

    added init.py files

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+"""
+Text-to-Calendar Event API Package
+"""
+
+__version__ = "1.0.0"
---
+++ api/app/__init__.py
@@ -0,0 +1,7 @@
+"""
+FastAPI application module for text-to-calendar event parsing.
+"""
+
+from .main import app
+
+__all__ = ["app"]
